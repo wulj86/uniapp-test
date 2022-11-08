7cdc5c0ad4bb0cd9ca76f2044a08e8abffda693d
wlj0923 <1152141896@qq.com>
2022-11-08 10:39:56 +0800

    init project

---
+++ components/testA.vue
@@ -0,0 +1,20 @@
+<template>
+	<view>
+		这是A组件
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"testA",
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
